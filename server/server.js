e1c80794678aa9c971a21d22b83d8127947140f6
JTrue <truehitt@gmail.com>
2023-06-13 11:14:08 -0400

    Setting up DB Set up DB with mongo. Added in connection file and linked to server. Wrapped app.listen in async function to confirm db successfully connects.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import connectDB from './config/connection.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,10 +8,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res) => {
-    res.status(404).send('404: Page not found');
+  res.status(404).send('404: Page not found');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}. Access site at http://localhost:${PORT}`);
-    }
-);
+(async () => {
+  try {
+    await connectDB();
+    console.log('Connected to database');
+    app.listen(PORT, () => {
+      console.log(
+        `Server listening on port ${PORT}. Access site at http://localhost:${PORT}`
+      );
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})();
